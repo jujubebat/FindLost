3cb192af98596ea5e865cc87816681710e3015ce
jujubebat <bbwwpark@naver.com>
2019-07-19 20:31:08 +0900

    db model 수정 작업

--- Main/models.py
+++ Main/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 # Create your models here.
 class LostItems(models.Model):
-    num = models.AutoField(primary_key=True)
-    itemName = models.CharField(db_column='itemName', max_length=100, blank=True, null=True)  # Field name made lowercase.
-    lostPlace = models.CharField(db_column='lostPlace', max_length=100, blank=True, null=True)  # Field name made lowercase.
+    num = models.AutoField(primary_key=True) #주키
+    managementID = models.CharField(db_column='managementID', max_length=100, blank=True, null=True) #관리ID
+    findYmd = models.CharField(db_column='findYmd', max_length=100, blank=True, null=True) #습득일자
+    productName = models.CharField(db_column='productName', max_length=100, blank=True, null=True) #물품명
+    keepPlace = models.CharField(db_column='keepPlace', max_length=100, blank=True, null=True) #보관장소
+    productImg = models.CharField(db_column='productImg', max_length=100, blank=True, null=True) #이미지
+    productDesc = models.CharField(db_column='productDesc', max_length=100, blank=True, null=True) #물품상세설명
+    productClass = models.CharField(db_column='productClass', max_length=100, blank=True, null=True) #물품분류명
+
+
+
+
 
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,14 @@
-본 프로젝트 케이싱 규칙
+<본 프로젝트 케이싱 규칙>
 -폴더명, 클래스, 함수 : 파스칼 케이싱
 -변수, 매개변수 : 카멜케이싱
 
 
+
+<데이터베이스 프로시저>
+CREATE PROCEDURE `del` ()
+BEGIN
+delete from findlost.main_lostitems;
+alter table findlost.main_lostitems auto_increment=0;
+END
+
+//프로시저를 사용하는 이유 .. 그냥 코드수 줄고 깔끔해서.. 딱히 그럴싸한 이유가 없음 사실
